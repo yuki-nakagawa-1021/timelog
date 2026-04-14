a63e544e28e92ff5c5196a52bfe2c9cfd6f354fc
nakagawa yuki <mstyk1021@gmail.com>
2026-04-15 00:05:34 +0900

    ログイン、会員登録、勤怠登録ページの画面設計

---
+++ src/resources/views/attendance/index.blade.php
@@ -0,0 +1,46 @@
+@extends('layouts.app')
+
+@section('content')
+<div>
+    <h2>
+        @if($status === 'before')
+            勤務外
+        @elseif($status === 'working')
+            出勤中
+        @elseif($status === 'break')
+            休憩中
+        @elseif($status === 'done')
+            退勤済
+        @endif
+    </h2>
+    <p>{{ now()->format('Y年m月d日') }}</p>
+    <p>{{ now()->format('H:i:s') }}</p>
+    <div>
+        @if($status === 'before')
+            <form method="POST" action="/attendance/start">
+                @csrf
+                <button type="submit">出勤</button>
+            </form>
+        @endif
+        @if($status === 'working')
+            <form method="POST" action="/attendance/break/start">
+                @csrf
+                <button type="submit">休憩入</button>
+            </form>
+            <form method="POST" action="/attendance/end">
+                @csrf
+                <button type="submit">退勤</button>
+            </form>
+        @endif
+        @if($status === 'break')
+            <form method="POST" action="/attendance/break/end">
+                @csrf
+                <button type="submit">休憩戻</button>
+            </form>
+        @endif
+    </div>
+    @if($status === 'done')
+        <p>お疲れ様でした。</p>
+    @endif
+</div>
+@endsection
