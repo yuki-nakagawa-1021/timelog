f034088bda9149c013db3ec510d043adede99bd7
nakagawa yuki <mstyk1021@gmail.com>
2026-04-15 02:32:31 +0900

    ログイン、会員登録ページのcss

--- src/resources/views/auth/register.blade.php
+++ src/resources/views/auth/register.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('css')
-<link rel="stylesheet" href="{{ ('css/login.css') }}">
+<link rel="stylesheet" href="{{ asset('css/auth/register.css') }}">
 @endsection
 
 @section('content')
